d9e442179cb8f377a2abfd86adafd79aee8034eb
thalesHDL <thales.dev.1501@gmail.com>
2021-02-25 10:35:58 -0300

    initial commit

---
+++ src/main/java/one/digital/inovation/diopersonapi/DioPersonapiApplication.java
@@ -0,0 +1,13 @@
+package one.digital.inovation.diopersonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioPersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioPersonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digital/inovation/diopersonapi/DioPersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digital.inovation.diopersonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioPersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
